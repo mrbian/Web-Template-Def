4d158956fb1c077ba376750187d682a30b473f36
卞留念 <554565808@qq.com>
2016-05-09 19:03:08 +0800

    添加侧边栏高亮

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by bian on 2016/4/25.
  */
+(function(){
+    var sidebar_num = $("#num").html();
+    var sidebar = $("#menu ul li");
+    sidebar = sidebar.splice(2,sidebar.length - 2);
+    for(var idx in sidebar){
+        if(idx === sidebar_num){
+            $(sidebar[idx]).addClass("active");
+            break;
+        }
+    }
+}());
